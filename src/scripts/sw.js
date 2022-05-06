77aeb0a5a701e05457cefa0565b43d25b6503e3c
M. Fajar Aditya <mfajaraditya13@gmail.com>
2022-05-06 08:50:59 +0800

    pwa final

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,16 +1,17 @@
+/* eslint-disable no-restricted-globals */
 import 'regenerator-runtime';
 import CacheHelper from './utils/cache-helper';
 
 const { assets } = global.serviceWorkerOption;
 
-this.addEventListener('install', (event) => {
+self.addEventListener('install', (event) => {
   event.waitUntil(CacheHelper.cachingAppShell([...assets, './']));
 });
 
-this.addEventListener('activate', (event) => {
+self.addEventListener('activate', (event) => {
   event.waitUntil(CacheHelper.deleteOldCache());
 });
 
-this.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(CacheHelper.revalidateCache(event.request));
 });
